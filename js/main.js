7285448c5beca5d197bab5ad4f9d9153c544c803
Azubu Ubuntu <chris.barnett@azubu.com>
2015-10-09 06:35:59 -0700

    initial push

---
+++ js/main.js
@@ -0,0 +1,24 @@
+(function() {
+  'use strict';
+
+  /**
+   * Hi! Thanks for using XSplit JS Framework :)
+   *
+   * XSplit JS Framework exposes useful methods to work with XSplit without 
+   * fiddling too much with the raw core methods exposed to JavaScript, which 
+   * makes developing plugins for XSplit a lot easier.
+   *
+   * To get started, you just have to require our library:
+   * var xjs = require('xjs');
+   *
+   * You can read more about it at our documentation:
+   * http://xjsframework.github.io/quickstart.html
+   * http://xjsframework.github.io/tutorials.html
+   * http://xjsframework.github.io/api.html
+   */
+  var xjs = require('xjs');
+
+  xjs.ready().then(function() {
+    // Write your code here
+  });
+})();
